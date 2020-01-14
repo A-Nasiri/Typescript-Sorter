5d8147a30c8371e5280b17c7d02443ff006fed60
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-01-14 18:43:20 +0000

    Sorting for array of numbers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hi there');
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < -i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
