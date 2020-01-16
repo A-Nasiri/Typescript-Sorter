06f5b0f17ac47451f51cbb6bcd6ba3341ef6f0db
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-01-16 05:26:36 +0000

    Refactored all data structure classes to become children of the abstract Sorter class

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,19 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-numbersCollection.sort();
-console.log(numbersCollection.data);
-
-// const numbersCollection = new NumbersCollection([100, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection('Xyaabj');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
+// charactersCollection.sort();
 // console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-3);
-// linkedList.add(-10);
-// linkedList.add(4);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-3);
+linkedList.add(-10);
+linkedList.add(4);
 
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
-// linkedList.print();
+linkedList.sort();
+linkedList.print();
