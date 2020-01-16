f5e9e34cba17bdbacb0d3439dd514f4404259a01
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-01-16 05:17:18 +0000

    Transformed Sorter into an abstract class

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
+const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
 // const numbersCollection = new NumbersCollection([100, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
@@ -13,12 +17,12 @@ import { LinkedList } from './LinkedList';
 // sorter.sort();
 // console.log(charactersCollection.data);
 
-const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(-3);
-linkedList.add(-10);
-linkedList.add(4);
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-3);
+// linkedList.add(-10);
+// linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
-linkedList.print();
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+// linkedList.print();
